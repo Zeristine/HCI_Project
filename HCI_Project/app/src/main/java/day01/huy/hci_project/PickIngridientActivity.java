b5cf53020a56fac3bb52092524e5110a8268bed4
Zeral <tsukaiyo@gmail.com>
2019-02-19 07:16:30 +0700

    Re-edit Pick Ingredient Activity

--- HCI_Project/app/src/main/java/day01/huy/hci_project/PickIngridientActivity.java
+++ HCI_Project/app/src/main/java/day01/huy/hci_project/PickIngridientActivity.java
@@ -2,11 +2,11 @@ package day01.huy.hci_project;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
-import android.widget.CheckBox;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.ListView;
@@ -15,7 +15,6 @@ import android.widget.Toast;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.stream.Stream;
 
 import day01.huy.hci_project.custom.TextAdapter;
 
@@ -45,8 +44,8 @@ public class PickIngridientActivity extends AppCompatActivity {
         List<String> ingredients = new ArrayList<>(mainIngredient);
         ingredients.addAll(subIngredient);
 
-        imgIcon1.setImageResource(R.drawable.icons_plus);
-        imgIcon2.setImageResource(R.drawable.icons_plus);
+        imgIcon1.setImageResource(R.drawable.icons_double_down);
+        imgIcon2.setImageResource(R.drawable.icons_double_down);
         btnSearch.setImageResource(R.drawable.icons_search);
         ArrayAdapter<String> adapter =
                 new ArrayAdapter<>(PickIngridientActivity.this, android.R.layout.simple_dropdown_item_1line,
@@ -56,10 +55,9 @@ public class PickIngridientActivity extends AppCompatActivity {
         txtIngredient.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String value = (String) parent.getItemAtPosition(position);
                 Toast.makeText(PickIngridientActivity.this,
-                        "The ingredient has been selected", Toast.LENGTH_SHORT).show();
-                String text = (String) parent.getItemAtPosition(position);
-                // ahihi
+                        "The ingredient " + value + " has been selected", Toast.LENGTH_SHORT).show();
             }
         });
         btnSearch.setOnClickListener(new View.OnClickListener() {
@@ -68,27 +66,31 @@ public class PickIngridientActivity extends AppCompatActivity {
 
             }
         });
-        lstSubIngredient.setAdapter(new TextAdapter(this, R.layout.layout_list_view_simple_row, subIngredient));
+
         lstMainIngredient.setAdapter(new TextAdapter(this, R.layout.layout_list_view_simple_row, mainIngredient));
+
+        lstSubIngredient.setAdapter(new TextAdapter(this, R.layout.layout_list_view_simple_row, subIngredient));
+
     }
 
     public void clickToShowMainList(View view) {
-        if(imgIcon1.getDrawable().getConstantState().equals(getDrawable(R.drawable.icons_plus).getConstantState())){
+        if (imgIcon1.getDrawable().getConstantState().equals(getDrawable(R.drawable.icons_double_down).getConstantState())) {
             lstMainIngredient.setVisibility(View.VISIBLE);
-            imgIcon1.setImageResource(R.drawable.icons_cancel);
-        }else{
+            imgIcon1.setImageResource(R.drawable.icons_double_up);
+        } else {
             lstMainIngredient.setVisibility(View.GONE);
-            imgIcon1.setImageResource(R.drawable.icons_plus);
+            imgIcon1.setImageResource(R.drawable.icons_double_down);
         }
     }
 
     public void clickToShowSubList(View view) {
-        if(imgIcon2.getDrawable().getConstantState().equals(getDrawable(R.drawable.icons_plus).getConstantState())){
+        if (imgIcon2.getDrawable().getConstantState().equals(getDrawable(R.drawable.icons_double_down).getConstantState())) {
             lstSubIngredient.setVisibility(View.VISIBLE);
-            imgIcon2.setImageResource(R.drawable.icons_cancel);
-        }else{
+            imgIcon2.setImageResource(R.drawable.icons_double_up);
+        } else {
             lstSubIngredient.setVisibility(View.GONE);
-            imgIcon2.setImageResource(R.drawable.icons_plus);
+            imgIcon2.setImageResource(R.drawable.icons_double_down);
         }
     }
+
 }
