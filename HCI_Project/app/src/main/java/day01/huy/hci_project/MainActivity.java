9acd6964053cff52dbe07b26bad46d596e1f18fd
Zeral <tsukaiyo@gmail.com>
2019-03-15 19:35:16 +0700

    change main to login

--- HCI_Project/app/src/main/java/day01/huy/hci_project/MainActivity.java
+++ HCI_Project/app/src/main/java/day01/huy/hci_project/MainActivity.java
@@ -1,36 +1,106 @@
 package day01.huy.hci_project;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.GridLayout;
 import android.view.View;
-import android.widget.Button;
+import android.view.WindowManager;
+import android.widget.EditText;
 import android.widget.ImageView;
-
-import day01.huy.hci_project.ultis.ColorGradient;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ImageView imgTitle;
-    private Button btnBegin;
+    private GridLayout layoutLoginChoice;
+    private LinearLayout layoutNormalLogin, layoutNormalRegister;
+    private TextView lblUsername, lblPassword, lblRUsername, lblRPassword, lblRConfirm;
+    private EditText txtUsername, txtPassword, txtRUsername, txtRPassword, txtRConfirm;
 
     @Override
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        imgTitle = findViewById(R.id.imgTitle);
-        btnBegin = findViewById(R.id.btnBegin);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        layoutLoginChoice = findViewById(R.id.layoutLoginChoice);
+        layoutNormalLogin = findViewById(R.id.layoutNormalLogin);
+        layoutNormalRegister = findViewById(R.id.layoutNormalRegister);
+        lblUsername = findViewById(R.id.lblUsername);
+        lblPassword = findViewById(R.id.lblPassword);
+        lblRUsername = findViewById(R.id.lblRUsername);
+        lblRPassword = findViewById(R.id.lblRPassword);
+        lblRConfirm = findViewById(R.id.lblRConfirm);
+        txtUsername = findViewById(R.id.txtUsername);
+        txtPassword = findViewById(R.id.txtPassword);
+        txtRUsername = findViewById(R.id.txtRUsername);
+        txtRPassword = findViewById(R.id.txtRPassword);
+        txtRConfirm = findViewById(R.id.txtRConfirm);
 
-        imgTitle.setImageResource(R.drawable.image_title);
+        lblUsername.setVisibility(View.INVISIBLE);
+        lblPassword.setVisibility(View.INVISIBLE);
+        lblRUsername.setVisibility(View.INVISIBLE);
+        lblRPassword.setVisibility(View.INVISIBLE);
+        lblRConfirm.setVisibility(View.INVISIBLE);
+        layoutNormalLogin.setVisibility(View.INVISIBLE);
+        layoutNormalRegister.setVisibility(View.INVISIBLE);
+        layoutLoginChoice.setVisibility(View.VISIBLE);
+    }
 
-        btnBegin.setBackground(ColorGradient.getRedGradientOrange(this));
-        btnBegin.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(MainActivity.this, NavigationActivity.class);
-                startActivity(intent);
-            }
-        });
+    public void clickToLogin(View view) {
+        //Move to Normal Login Form
+        if (layoutLoginChoice.getVisibility() == View.VISIBLE) {
+            layoutLoginChoice.setVisibility(View.INVISIBLE);
+            layoutNormalLogin.setVisibility(View.VISIBLE);
+            return;
+        }
+        //Login
+        if (layoutNormalLogin.getVisibility() == View.VISIBLE) {
+            //Do Login
+            return;
+        }
+    }
+
+    public void clickToRegister(View view) {
+        //Move to Register Form
+        if (layoutNormalLogin.getVisibility() == View.VISIBLE) {
+            layoutNormalLogin.setVisibility(View.INVISIBLE);
+            layoutNormalRegister.setVisibility(View.VISIBLE);
+            return;
+        }
+        //Register After Register Reappear Login
+        if (layoutNormalRegister.getVisibility() == View.VISIBLE) {
+            //Do Register
+            txtUsername.setText(txtRUsername.getText().toString());
+            txtPassword.setText(txtRPassword.getText().toString());
+            txtRUsername.setText("");
+            txtRPassword.setText("");
+            layoutNormalRegister.setVisibility(View.INVISIBLE);
+            layoutNormalLogin.setVisibility(View.VISIBLE);
+        }
+    }
+
+    public void clickToBack(View view) {
+        //Back from Register form
+        if (layoutNormalRegister.getVisibility() == View.VISIBLE &&
+                layoutNormalLogin.getVisibility() == View.INVISIBLE &&
+                layoutLoginChoice.getVisibility() == View.INVISIBLE
+        ) {
+            layoutNormalRegister.setVisibility(View.INVISIBLE);
+            layoutLoginChoice.setVisibility(View.INVISIBLE);
+            layoutNormalLogin.setVisibility(View.VISIBLE);
+            return;
+        }
+        //Back from Login form
+        if (layoutNormalLogin.getVisibility() == View.VISIBLE &&
+                layoutNormalRegister.getVisibility() == View.INVISIBLE &&
+                layoutLoginChoice.getVisibility() == View.INVISIBLE
+        ) {
+            layoutNormalRegister.setVisibility(View.INVISIBLE);
+            layoutLoginChoice.setVisibility(View.VISIBLE);
+            layoutNormalLogin.setVisibility(View.INVISIBLE);
+            return;
+        }
     }
 }
 
