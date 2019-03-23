b711a548d90bd2156189b599d8af8101e44574be
Zeral <tsukaiyo@gmail.com>
2019-03-23 09:48:58 +0700

    update fridge

--- HCI_Project/app/src/main/java/day01/huy/hci_project/FridgeActivity.java
+++ HCI_Project/app/src/main/java/day01/huy/hci_project/FridgeActivity.java
@@ -4,8 +4,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.LinearLayout;
 
 public class FridgeActivity extends AppCompatActivity {
+
+    private LinearLayout layoutAddIngredient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,9 +16,6 @@ public class FridgeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_fridge);
     }
 
-    public void clickToConfirm(View view) {
-        Intent intent = new Intent(this, NavigationActivity.class);
-        startActivity(intent);
-        finish();
+    public void clickToAddIngredient(View view) {
     }
 }
